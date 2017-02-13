d85f55e563ce2e09a59ac3306b68f1be44f2e66b
Richard Cavell <richardcavell@mail.com>
2017-02-13 01:45:34 +0000

    Add error.c, error.h Signed-off-by: Richard Cavell <richardcavell@mail.com>

---
+++ Include/error.h
@@ -0,0 +1,11 @@
+/* wrap by Richard Cavell v1.0 */
+/* January-February 2017 */
+/* error.h */
+
+#ifndef ERROR_H
+#define ERROR_H
+
+extern void xprintf(const char *s, ...);
+extern void fail_msg(const char *s, ...);
+
+#endif
---
+++ Source/error.c
@@ -0,0 +1,38 @@
+/* wrap by Richard Cavell v1.0 */
+/* January-February 2017 */
+/* error.c */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+#include "error.h"
+
+void
+xprintf(const char *s, ...)
+{
+  int return_code;
+  va_list ap;
+
+  va_start(ap, s);
+  return_code = vprintf(s, ap);
+  va_end(ap);
+
+  if (return_code < 0)
+  {
+    fprintf(stderr, "Unable to print to standard output\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void
+fail_msg(const char *s, ...)
+{
+  va_list ap;
+
+  va_start(ap, s);
+  vfprintf(stderr, s, ap);
+  va_end(ap);
+
+  exit(EXIT_FAILURE);
+}
