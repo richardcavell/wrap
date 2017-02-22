0ea1624635df74411c71e471d6fbe56e9599821b
Richard Cavell <richardcavell@mail.com>
2017-02-22 17:21:33 +0000

    Add comment in error.h Signed-off-by: Richard Cavell <richardcavell@mail.com>

--- Include/error.h
+++ Include/error.h
@@ -5,7 +5,7 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-extern void xprintf(const char *s, ...);
+extern void xprintf(const char *s, ...);   /* wrapper for printf() */
 extern void xerror(const char *s, ...);    /* this returns to the caller */
 extern void fail_msg(const char *s, ...);  /* this exits with EXIT_FAILURE */
 
