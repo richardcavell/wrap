ce9f8f9a3bc1c09cc0e0409506f9e656bf40f8d9
Richard Cavell <richardcavell@mail.com>
2017-05-05 20:02:37 +0100

    Fixed the help text to remove ugly code Signed-off-by: Richard Cavell <richardcavell@mail.com>

--- Include/config.h
+++ Include/config.h
@@ -7,27 +7,25 @@
 
 #include <limits.h>
 
-#define _makestr(a) #a
-#define makestr(a) _makestr(a)
-
   /* User-modifiable values */
 #define BUFFER_MIN 100
 #define DEFAULT_BUFFER_SIZE 1024
-#define DEFAULT_BUFFER_SIZE_TEXT makestr(DEFAULT_BUFFER_SIZE)
 #define BUFFER_MAX INT_MAX
 
+#define COLS 80
 #define LENGTH_MIN 5
 #define DEFAULT_LINE_LENGTH 20
-#define DEFAULT_LINE_LENGTH_TEXT makestr(DEFAULT_LINE_LENGTH)
 #define LENGTH_MAX 100
 
 #define STOPS_MIN 1
 #define DEFAULT_STOPS 8
-#define DEFAULT_STOPS_TEXT makestr(DEFAULT_STOPS)
 #define STOPS_MAX 100
 
 #define HELP_DIVIDER1 5
 #define HELP_DIVIDER2 22
+
+/* Right edge - left edge - 5 for prettiness + 2 for the \n and \0 */
+#define HELP_TEXT_SCRATCH_SIZE (COLS - HELP_DIVIDER2 - 5 + 2)
 
 #define DEFAULT_INVOCATION "wrap"
 #define DEFAULT_ALWAYS_HYPHENATE 0
--- Source/options.c
+++ Source/options.c
@@ -8,10 +8,9 @@
 #include <limits.h>
 #include <assert.h>
 
-#include "options.h"
-
 #include "config.h"
 #include "error.h"
+#include "options.h"
 
 static const struct options_type default_options =
 {
@@ -23,67 +22,68 @@ static const struct options_type default_options =
   DEFAULT_ALWAYS_HYPHENATE     /* config.h */
 };
 
-typedef void option_fn(const char *param_match, const char *param_remainder,
+typedef void option_fn(const char *param_match,
+                       const char *param_remainder,
                        struct options_type *options);
 
-static option_fn option_always_hyphenate,
+static option_fn option_line_length,
+                 option_always_hyphenate,
                  option_line_break,
-                 option_buffer,
-                 option_length,
                  option_stops,
+                 option_buffer_size,
                  print_help,
                  print_version;
+
+typedef void option_ht(char *s, size_t len);
+
+static option_ht option_line_length_helptext,
+                 option_always_hyphenate_helptext,
+                 option_line_break_helptext,
+                 option_stops_helptext,
+                 option_buffer_size_helptext,
+                 print_help_helptext,
+                 print_version_helptext;
 
 struct parameter_type
 {
   const char *short_name, *long_name;
-  const char *help_text;
+  option_ht *ht;
   option_fn *fn;
 };
 
 static const struct parameter_type parameters[] =
 {
+  { "-l=", "--line-length=",
+    option_line_length_helptext,
+    option_line_length } ,
+
   { "-a",  "--always-hyphenate",
-    "Fill up each line"
-#if (DEFAULT_ALWAYS_HYPHENATE)
-        " (default)" 
-#else
-        " (switches off -k)"
-#endif
-        ,
+    option_always_hyphenate_helptext,
     option_always_hyphenate } ,
 
-  { "-b=", "--buffer-size=", 
-    "Buffer size (default "
-      DEFAULT_BUFFER_SIZE_TEXT
-      ")",
-    option_buffer } ,
-
   { "-k",  "--line-break",
-    "Line-break after whole words"
-#if (DEFAULT_ALWAYS_HYPHENATE == 0)
-        " (default)"
-#else
-        " (switches off -a)"
-#endif
-        ,
+    option_line_break_helptext,
     option_line_break } ,
 
-  { "-l=", "--line-length=",
-    "Line length (default "
-      DEFAULT_LINE_LENGTH_TEXT
-      ")",
-    option_length } ,
-
   { "-s=", "--stops=",
-    "Tab stop distance (default "
-      DEFAULT_STOPS_TEXT
-      ")",
+    option_stops_helptext,
     option_stops } ,
 
-  { "-h",  "--help",         "Prints out this help text", print_help } ,
-  { "-v",  "--version",      "Version number",            print_version } ,
-  { NULL,  NULL,             NULL,                        NULL }
+  { "-b=", "--buffer-size=", 
+    option_buffer_size_helptext,
+    option_buffer_size } ,
+
+  { "-h",  "--help",
+    print_help_helptext,
+    print_help } ,
+
+  { "-v",  "--version",
+    print_version_helptext,
+    print_version } ,
+
+  { NULL,  NULL,
+    NULL,
+    NULL }
 };
 
 static int
@@ -155,24 +155,16 @@ static unsigned int get_ui(const char *param_remainder,
                            const char *param_match,
                            unsigned int min, unsigned int max);
 
+static void check_param_finished(const char *param_match,
+                                 const char *param_remainder);
+
 static void
-option_length(const char *param_match, const char *param_remainder,
+option_line_length(const char *param_match, const char *param_remainder,
               struct options_type *options)
 {
   options->line_length = get_ui(param_remainder, param_match,
                                LENGTH_MIN, LENGTH_MAX); /* macros from config.h */
 }
-
-static void
-option_stops(const char *param_match, const char *param_remainder,
-             struct options_type *options)
-{
-  options->stops = get_ui(param_remainder, param_match,
-                         STOPS_MIN, STOPS_MAX); /* macros from config.h */
-}
-
-static void check_param_finished(const char *param_match,
-                                 const char *param_remainder);
 
 static void
 option_always_hyphenate(const char *param_match,
@@ -192,8 +184,16 @@ option_line_break(const char *param_match, const char *param_remainder,
 }
 
 static void
-option_buffer(const char *param_match, const char *param_remainder,
-              struct options_type *options)
+option_stops(const char *param_match, const char *param_remainder,
+             struct options_type *options)
+{
+  options->stops = get_ui(param_remainder, param_match,
+                         STOPS_MIN, STOPS_MAX); /* macros from config.h */
+}
+
+static void
+option_buffer_size(const char *param_match, const char *param_remainder,
+                   struct options_type *options)
 {
   assert(BUFFER_MAX <= UINT_MAX); /* macros are from config.h */
   options->buffer_size = get_ui(param_remainder, param_match,
@@ -205,6 +205,7 @@ print_help(const char *param_match, const char *param_remainder,
            struct options_type *options)
 {
   const struct parameter_type *param = parameters;
+  char help_text[HELP_TEXT_SCRATCH_SIZE];
 
   check_param_finished(param_match, param_remainder);
 
@@ -214,10 +215,14 @@ print_help(const char *param_match, const char *param_remainder,
 
   for (;param->short_name; ++param)
   {
+    /* void. Returns a payload in help_text */
+    param->ht(help_text, sizeof(help_text));
+
     xprintf(" %-*sor  %-*s%s\n",
               HELP_DIVIDER1, param->short_name,
               HELP_DIVIDER2, param->long_name,
-              param->help_text);
+              help_text
+           );
   }
 
   exit(EXIT_SUCCESS);
@@ -265,4 +270,85 @@ check_param_finished(const char *param_match, const char *param_remainder)
     fail_msg(
       "Error: Parameter %s should not be followed by any other text\n",
                         param_match);
+}
+
+static void
+option_line_length_helptext(char *s, size_t len)
+{
+  if (
+       snprintf(s, len,
+                "Line length (default %u)",
+                DEFAULT_LINE_LENGTH
+               )
+        < 0)
+
+    fail_msg("snprintf() failure in option_line_length_helptext()");
+}
+
+static void
+option_always_hyphenate_helptext(char *s, size_t len)
+{
+  if (
+       snprintf(s, len,
+                "Fill up each line %s",
+                (DEFAULT_ALWAYS_HYPHENATE) ?
+                  "(default)" : "(switches off -k)"
+               )
+        < 0)
+
+    fail_msg("snprintf() failure in option_always_hyphenate_helptext()");
+}
+
+static void
+option_line_break_helptext(char *s, size_t len)
+{
+  if (
+       snprintf(s, len,
+                "Line-break after whole words %s",
+                  DEFAULT_ALWAYS_HYPHENATE == 0 ?
+                  "(default)" : "(switches off -a)"
+               )
+        < 0)
+
+    fail_msg("snprintf() failure in option_line_break_helptext()");
+}
+
+static void
+option_stops_helptext(char *s, size_t len)
+{
+  if (
+       snprintf(s, len,
+                "Tab stop distance (default %u)",
+                DEFAULT_STOPS
+               )
+        < 0)
+
+    fail_msg("snprintf() failure in option_stops_helptext()");
+}
+
+static void
+option_buffer_size_helptext(char *s, size_t len)
+{
+  if (
+       snprintf(s, len,
+                "Buffer size (default %u)",
+                DEFAULT_BUFFER_SIZE
+               )
+        < 0)
+
+    fail_msg("snprintf() failure in option_buffer_size_helptext()");
+}
+
+static void
+print_help_helptext(char *s, size_t len)
+{
+  if (snprintf(s, len, "Prints out this help text") < 0)
+    fail_msg("snprintf() failure in option_help_helptext()");
+}
+
+static void
+print_version_helptext(char *s, size_t len)
+{
+  if (snprintf(s, len, "Version number") < 0)
+    fail_msg("snprintf() failure in option_version_helptext()");
 }
