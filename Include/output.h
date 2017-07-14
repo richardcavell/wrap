5e72f479d3ad85170c85c1a5d34eefc2f0e00db9
Richard Cavell <richardcavell@mail.com>
2017-07-14 15:02:02 +0100

    Should have used git mv
    Signed-off-by: Richard Cavell <richardcavell@mail.com>

---
+++ Include/output.h
@@ -0,0 +1,14 @@
+/* Wrap by Richard Cavell v1.0 */
+/* Copyright (c) 2017 Richard John Foster Cavell */
+/* output.h */
+
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+extern void xprintf(const char *s, ...);   /* wrapper for printf() */
+extern void xerror(const char *s, ...);    /* this prints to stderr and then
+                                              returns to the caller */
+extern void fail_msg(const char *s, ...);  /* this prints to stderr and then
+                                              exits with EXIT_FAILURE */
+
+#endif
