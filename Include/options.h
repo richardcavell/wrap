4b60dde1b034e7223d3408953584d111626848da
Richard Cavell <richardcavell@mail.com>
2017-07-30 16:17:04 +0100

    Change options.h formatting to be consistent with other files.
    Signed-off-by: Richard Cavell <richardcavell@mail.com>

--- Include/options.h
+++ Include/options.h
@@ -10,11 +10,11 @@
 struct options_type
 {
   const char *invocation;  /* The name of this program, from the command line */
-  int file_parameters;  /* Either 0 (absent) or 1 (present) */
+  int file_parameters;     /* Either 0 (absent) or 1 (present) */
   size_t buffer_size;
   unsigned int line_length;
   unsigned int stops;
-  int always_hyphenate; /* Either 0 or 1 */
+  int always_hyphenate;    /* Either 0 or 1 */
 };
 
 /* These two return 0 or 1 */
@@ -27,6 +27,6 @@ extern int is_stdin(const char *arg);
 */
 
 extern struct options_type
-         get_options(int argc, char *argv[]);
+get_options(int argc, char *argv[]);
 
 #endif
